08151ebb1a518d54b1c0491fa6edf0681a0753db
michael-andreuzza <michaelandreuzzavega@gmail.com>
2020-09-23 13:33:26 +0300

    pricing

--- tailwind.config.js
+++ tailwind.config.js
@@ -189,7 +189,7 @@ module.exports = {
     borderRadius: {
       none: '0',
       sm: '0.125rem',
-      default: '0.25rem',
+      default: '1rem',
       md: '0.375rem',
       lg: '0.5rem',
       full: '9999px',
