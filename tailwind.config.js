7a52a6e59fd36800f6764ffe9b7ee1c6962ac49d
michael-andreuzza <michaelandreuzzavega@gmail.com>
2020-12-10 18:04:23 +0200

    added 60 sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -865,5 +865,8 @@ module.exports = {
     wordBreak: ['responsive'],
     zIndex: ['responsive', 'focus-within', 'focus'],
   },
-  plugins: [],
+ plugins: [
+    require('@tailwindcss/forms'),
+    // ...
+  ],
 }
